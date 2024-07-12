f15afb64174eee0be2218c310d80bca6eed7cfb6
kazu0 <kazuooficial@hotmail.com>
2024-07-12 17:33:21 -0300

    see you in hell

--- HELL-INVADER/src/main/GameFrame.java
+++ HELL-INVADER/src/main/GameFrame.java
@@ -4,8 +4,13 @@ import javax.swing.*;
 
 public class GameFrame {
     public GameFrame() {
+        ImageIcon icon = new ImageIcon("res/icon/icon.png");
+
         GamePanel gp = new GamePanel();
         JFrame frame = new JFrame("HELL INVADER");
+
+        frame.setIconImage(icon.getImage());
+
         frame.add(gp);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
